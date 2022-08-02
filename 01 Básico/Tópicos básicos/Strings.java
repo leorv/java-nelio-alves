df9d76641d48fcdf7e053ead4fdcaa7428077a10
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-08-01 21:30:40 -0300

    fix in Strings

--- 01 Básico/Tópicos básicos/Strings.java
+++ 01 Básico/Tópicos básicos/Strings.java
@@ -12,28 +12,29 @@ public class Strings {
         int i = original.indexOf("bc");
         int j = original.lastIndexOf("bc");
 
-        System.out.println("-" + original + "-");
-        System.out.println("-" + s01 + "-");
-        System.out.println("-" + s02 + "-");
-        System.out.println("-" + s03 + "-");
-        System.out.println("-" + s04 + "-");
-        System.out.println("-" + s05 + "-");
-        System.out.println("-" + s06 + "-");
-        System.out.println("-" + s07 + "-");
-        System.out.println("-" + i + "-");
-        System.out.println("-" + j + "-");
+        System.out.println("Original-" + original + "-");
+        System.out.println("toLowerCase-" + s01 + "-");
+        System.out.println("toUpperCase-" + s02 + "-");
+        System.out.println("trim-" + s03 + "-");
+        System.out.println("substring-" + s04 + "-");
+        System.out.println("substring-" + s05 + "-");
+        System.out.println("replace-" + s06 + "-");
+        System.out.println("replace-" + s07 + "-");
+        System.out.println("indexOf-" + i + "-");
+        System.out.println("lastIndexOf-" + j + "-");
 
         // Split
         String s = "potato apple lemon";
 
-        String[] vect = s.split(s);
+        String[] vect = s.split(" ");
         String word1 = vect[0];
         String word2 = vect[1];
         String word3 = vect[2];
 
-        System.out.println(word1);
-        System.out.println(word2);
-        System.out.println(word3);
+        System.out.println("word1: " + word1);
+        System.out.println("word2: " + word2);
+        System.out.println("word3: " + word3);
+        System.out.println("vect: " + vect);
 
     }
 }
